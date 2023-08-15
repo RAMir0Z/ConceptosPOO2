5435d68fa0092cfdc9222d5332417e25c143e25c
RAMir0Z <r4mirodesp4@gmail.com>
2023-08-15 11:41:28 -0600

    Finish day class

---
+++ ConceptPOO/ConceptsPOO2I/ConceptsPOO2I/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConceptsPOO2I;
+
+Console.WriteLine("Concepts POO 2do Intento");
+Console.WriteLine("========================");
+
+try
+{
+	Console.WriteLine(new Date(2023, 8, 15));
+	Console.WriteLine(new Date(2012, 12, 1));
+	Console.WriteLine(new Date(2000, 2, 29));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
