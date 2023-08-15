09f8cbc8ee9bf0616d617572b90814c59e6b9ce5
RAMir0Z <r4mirodesp4@gmail.com>
2023-08-15 13:19:33 -0600

    2do avance.

---
+++ ConceptPOO/ConceptsPOO2I/ConceptsPOO2I/ComissionEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO2I
+{
+    public class ComissionEmployee : Employee
+    {
+        public float ComissionPercentaje { get; set; }
+        public decimal Sales { get; set; }
+        public override decimal GetValueToPay()
+        {
+            return Sales * (decimal)ComissionPercentaje;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tComission percentaje: {$"{ComissionPercentaje:P2}", 18}" +
+                $"\n\tSales...............: {$"{Sales:C2}",18}" +
+                $"\n\tValue to pay........: {$"{GetValueToPay():C2}",18}";
+        }
+    }
+}
--- ConceptPOO/ConceptsPOO2I/ConceptsPOO2I/Program.cs
+++ ConceptPOO/ConceptsPOO2I/ConceptsPOO2I/Program.cs
@@ -13,3 +13,28 @@ catch (Exception error)
 {
 	Console.WriteLine(error.Message);
 }
+
+Employee e1 = new SalaryEmployee()
+{
+	Id = 1,
+	FirstName = "Ana",
+	LastName = "Valero",
+	BirthDate = new Date(1995, 5, 26),
+	HiringDate = new Date(2020, 9, 1),
+	IsActive = true,
+	Salary = 25000M
+};
+
+Employee e2 = new ComissionEmployee()
+{
+    Id = 2,
+    FirstName = "Ricardo",
+    LastName = "Arjona",
+    BirthDate = new Date(1985, 3, 15),
+    HiringDate = new Date(2021, 2, 1),
+    IsActive = true,
+    Sales = 1500000M,
+	ComissionPercentaje = 0.05F
+};
+Console.WriteLine(e1);
+Console.WriteLine(e2);
